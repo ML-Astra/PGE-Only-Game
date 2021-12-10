c38c4b22ea845a721664bd8fd3e3500354debb81
ML-Astra <zer0lexaros@gmail.com>
2021-12-10 04:00:50 +0000

    Comments Comments Comments, or i'll get lost

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #define OLC_PGE_APPLICATION
 #include <iostream>
-#include "olcPixelGameEngine.h"
+#include "Headers/olcPixelGameEngine.h"
 
 class PGE001 : public olc::PixelGameEngine {
 public:
 	PGE001() {
-		// I really need to change this at somepoint
+		// TODO:I really need to change this at somepoint
 		sAppName = "PGE1";
 	};
 public:
@@ -15,15 +15,17 @@ public:
 	};
 	// On Every Frame
 	bool OnUserUpdate(float fTime) override {
+		// Generates a colourful static image
 		for (int x = 0; x < ScreenWidth(); x++)
 			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+				Draw(x, y, olc::Pixel(rand() % 63-25+1, rand() % 256, rand() % 256));
 
 		return true;
 	}
 };
 
 int main() {
+	// Create an instance of PGE
 	PGE001 app;
 	// Width, Height, Scale X, Scale Y
 	if (app.Construct(256, 240, 4, 4))
